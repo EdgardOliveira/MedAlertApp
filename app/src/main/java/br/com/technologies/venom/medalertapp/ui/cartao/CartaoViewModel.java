e228782a6680b0554eae241e049698b5f8c50973
Thiago Lins <xthiagolins@gmail.com>
2021-10-21 00:04:41 -0400

    feat: Exibir dados do cartão do usuário
    - Atualiza layout para mostrar os dados do cliente
    - Recupera os dados do paciente do repositório
    - Exibe os dados do paciente na tela do cartão

--- app/src/main/java/br/com/technologies/venom/medalertapp/ui/cartao/CartaoViewModel.java
+++ app/src/main/java/br/com/technologies/venom/medalertapp/ui/cartao/CartaoViewModel.java
@@ -1,7 +1,30 @@
 package br.com.technologies.venom.medalertapp.ui.cartao;
 
-import androidx.lifecycle.ViewModel;
+import android.app.Application;
 
-public class CartaoViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import br.com.technologies.venom.medalertapp.AppRepository;
+import br.com.technologies.venom.medalertapp.models.Paciente;
+
+public class CartaoViewModel extends AndroidViewModel {
+
+    private AppRepository appRepository;
+    private LiveData<List<Paciente>> pacientes;
+
+    public CartaoViewModel(@NonNull Application application) {
+        super(application);
+        appRepository = new AppRepository(getApplication());
+
+        appRepository.obterConsultasAPI();
+        pacientes = appRepository.listarPacientes();
+    }
+
+    public LiveData<List<Paciente>> getPaciente() {
+        return pacientes;
+    }
 }
